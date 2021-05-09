74f9b1e70a369bbdf6c6cddc15baaeffc8aefaaa
qing <qingping1998@gmail.com>
2021-05-09 23:31:12 +0800

    Fix change for GenericManageUserActivity

--- app/src/main/java/com/example/csit314_project/GenericManageUserActivity.java
+++ app/src/main/java/com/example/csit314_project/GenericManageUserActivity.java
@@ -11,32 +11,43 @@ public class GenericManageUserActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.generic_manageuser);
         // TODO manageuser work here
-        Button btn_back = findViewById(R.id.btn_back);
-        btn_back.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-            //back btn interaction here
 
+        Button btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //back btn interaction here
+                //TODO transition back to the calling activity, could be healthorg or healthstaff @qing
             }
-        }
+        });
         
         Button btn_suspend = findViewById(R.id.btn_suspend);
-        btn_suspend.setOnClickListener(new View.OnClickListener(){
+        btn_suspend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                //back btn interaction here
-
+            public void onClick(View view) {
+                //suspend btn interaction her
             }
-        }
+        });
         Button btn_sendAlert = findViewById(R.id.btn_sendAlert);
-        btn_sendAlert.setOnClickListener(new View.OnClickListener(){
+        btn_sendAlert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                //back btn interaction here
-
+            public void onClick(View view) {
+                //sendAlert btn interaction here
             }
+        });
+
+        //RECALL: DIFFERENT USER TYPES HAS ACCESS TO DIFFERENT BUTTONS
+        UserController UC = UserController.getInstance();
+        if (UC.currentUser.role.equals("Health_Org")) {
+            //set visibility of button SUSPEND
+            btn_suspend.setVisibility(View.VISIBLE);
         }
+        else if (UC.currentUser.role.equals("Health_Staff")) {
+            //set visibility of button SEND ALERT
+            btn_sendAlert.setVisibility(View.VISIBLE);
+        }
+
+        //
+
     }
-
-
 }
