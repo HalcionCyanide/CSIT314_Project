fa3f1af4c7cc2e6c79b2267ded7b30a7dce02033
DESKTOP-FNHP50K\Tempura <eggtart1628@gmail.com>
2021-05-10 22:29:12 +0800

    added new classes(Alert, Employment, Vaccination)

---
+++ app/src/main/java/com/example/csit314_project/Employment.java
@@ -0,0 +1,6 @@
+package com.example.csit314_project;
+
+public class Employment {
+    String NRIC;
+    String employement_location;
+}
---
+++ app/src/main/java/com/example/csit314_project/Vaccination.java
@@ -0,0 +1,8 @@
+package com.example.csit314_project;
+
+public class Vaccination {
+    String NRIC;
+    String vaccination_brand;
+    String first_vaccination;
+    String second_vaccination;
+}
