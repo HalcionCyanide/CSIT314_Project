f4e421eb4c5f0a93b7247610111052bebff59969
DESKTOP-VAG019V\eggta <eggtart1628@gmail.com>
2021-05-25 18:06:26 +0800

    Added HealthOrgLogin TDD

--- app/src/androidTest/java/com/example/csit314_project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/csit314_project/ExampleInstrumentedTest.java
@@ -1,13 +1,37 @@
 package com.example.csit314_project;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Context;
+import android.content.Intent;
 
+
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import junit.framework.AssertionFailedError;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.VerificationModes.times;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +46,75 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.csit314_project", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityTestRule =
+            new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void healthOrgSuccessfulLogin() {
+
+        Intents.init();
+
+        onView(withId(R.id.txt_username)).perform(typeText("admin"));
+        onView(withId(R.id.txt_password)).perform(typeText("123")).perform(closeSoftKeyboard());
+        onView(withId(R.id.btn_login)).perform(click());
+
+        activityTestRule.launchActivity(new Intent());
+        intended(hasComponent(HealthOrgMainActivity.class.getName()));
+
+        Intents.release();
+    }
+
+    @Test
+    public void healthOrgWrongPasswordLogin() {
+
+        Intents.init();
+
+        onView(withId(R.id.txt_username)).perform(typeText("admin"));
+        onView(withId(R.id.txt_password)).perform(typeText("123a")).perform(closeSoftKeyboard());
+        onView(withId(R.id.btn_login)).perform(click());
+
+        activityTestRule.launchActivity(new Intent());
+        intended(hasComponent(LoginActivity.class.getName()));
+        intended(hasComponent(HealthOrgMainActivity.class.getName()), times(0));
+
+        Intents.release();
+
+    }
+
+    @Test
+    public void healthOrgWrongUsernameLogin() {
+
+        Intents.init();
+
+        onView(withId(R.id.txt_username)).perform(typeText("admina"));
+        onView(withId(R.id.txt_password)).perform(typeText("123")).perform(closeSoftKeyboard());
+        onView(withId(R.id.btn_login)).perform(click());
+
+        activityTestRule.launchActivity(new Intent());
+        intended(hasComponent(LoginActivity.class.getName()));
+        intended(hasComponent(HealthOrgMainActivity.class.getName()), times(0));
+
+        Intents.release();
+
+    }
+
+    @Test
+    public void healthOrgNoValuesLogin() {
+
+        Intents.init();
+
+        onView(withId(R.id.txt_username)).perform(typeText(""));
+        onView(withId(R.id.txt_password)).perform(typeText("")).perform(closeSoftKeyboard());
+        onView(withId(R.id.btn_login)).perform(click());
+
+        activityTestRule.launchActivity(new Intent());
+        intended(hasComponent(LoginActivity.class.getName()));
+        intended(hasComponent(HealthOrgMainActivity.class.getName()), times(0));
+
+        Intents.release();
+
     }
 }
